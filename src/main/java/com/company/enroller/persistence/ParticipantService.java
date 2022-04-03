771bed49cfbc8f9ad6d511531904bdea0fb06043
dmszoltysek <dm.szoltysek@gmail.com>
2022-04-03 15:25:05 +0200

    User endpoints created.

--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,8 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Session;
+import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 import org.springframework.stereotype.Component;
 
@@ -21,5 +23,28 @@ public class ParticipantService {
 		Query query = connector.getSession().createQuery(hql);
 		return query.list();
 	}
+
+	public Participant findByLogin(String login){
+		return (Participant) connector.getSession().get(Participant.class, login);
+	}
+
+	public void delete(Participant participant){
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();
+	}
+
+	public void register(Participant participant){
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+	}
+
+	public void update(Participant participant){
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().merge(participant);
+		transaction.commit();
+	}
+
 
 }
